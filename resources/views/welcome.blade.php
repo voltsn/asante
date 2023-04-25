239fbecc1aad3f24b9bbd23f3162acf1cf888586
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-04-25 14:23:17 +0200

    Add css classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,76 +12,76 @@
         <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     </head>
     <body>
-        <header>
-            <div>AS</div>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Gallery</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><button>Get tickets</button></li>
+        <header class="main-header">
+            <div class="main-header__logo">AS</div>
+            <nav class="main-header__nav">
+                <ul class="main-header__nav__menu">
+                    <li class="main-header__nav__menu__item"><a href="#">Home</a></li>
+                    <li class="main-header__nav__menu__item"><a href="#">About</a></li>
+                    <li class="main-header__nav__menu__item"><a href="#">Gallery</a></li>
+                    <li class="main-header__nav__menu__item"><a href="#">Contact</a></li>
+                    <li class="main-header__nav__menu__item"><button class="btn btn--yellow btn-sm">Get tickets</button></li>
                 </ul>
             </nav>
-            <button>
-                <span></span>
-                <span></span>
-                <span></span>
+            <button class="toggle-nav">
+                <span class="toggle-nav__line top"></span>
+                <span class="toggle-nav__line middle"></span>
+                <span class="toggle-nav__line bottom"></span>
             </button>
         </header>
         <main>
-            <section>
-                <h1>Asante</h1>
-                <p>
+            <section class="hero">
+                <h1 class="hero__heading">Asante</h1>
+                <p class="hero__description">
                     Discover the art of abstraction like you never have before at our brand new exhibition. <button>Get tickets now</button>
                 </p>
-                <div>
-                    <img src="/images/pexels-steve-johnson-1789968.png" alt="abstract art" aria-hidden="true">
-                    <img src="/images/pexels-paul-blenkhorn-sensoryarthouse-2964362.png" alt="abstract art" aria-hidden="true">
-                    <img src="/images/pexels-steve-johnson-13612602.png" alt="abstract art" aria-hidden="true">
-                    <img src="/images/pexels-steve-johnson-1266808.png" alt="abstract art" aria-hidden="true">
-                    <img src="/images/pexels-suzy-hazelwood-1762973.png" alt="abstract art" aria-hidden="true">
+                <div class="hero__images">
+                    <img class="hero__images__image" src="/images/pexels-steve-johnson-1789968.png" alt="abstract art" aria-hidden="true">
+                    <img class="hero__images__image" src="/images/pexels-paul-blenkhorn-sensoryarthouse-2964362.png" alt="abstract art" aria-hidden="true">
+                    <img class="hero__images__image" src="/images/pexels-steve-johnson-13612602.png" alt="abstract art" aria-hidden="true">
+                    <img class="hero__images__image" src="/images/pexels-steve-johnson-1266808.png" alt="abstract art" aria-hidden="true">
+                    <img class="hero__images__image" src="/images/pexels-suzy-hazelwood-1762973.png" alt="abstract art" aria-hidden="true">
                 </div>
             </section>
-            <section>
-                <h2>
+            <section class="about">
+                <h2 class="about__heading">
                     Welcome to Asante, we are a collective of creatives, whose mission is to make art accessible to everyone.
                 </h2>
 
-                <div>
-                    <div>
-                        <img src="/images/pexels-steve-johnson-13612602.png" alt="abstract art" aria-hidden="true">
-                        <img src="/images/pexels-steve-johnson-1266808.png" alt="abstract art" aria-hidden="true">
-                        <img src="/images/pexels-steve-johnson-1789968.png" alt="abstract art" aria-hidden="true">
+                <div class="about__exhibition-latest">
+                    <div class="about__exhibition-latest__images">
+                        <img class="about__exhibition-latest__images__image" src="/images/pexels-steve-johnson-13612602.png" alt="abstract art" aria-hidden="true">
+                        <img class="about__exhibition-latest__images__image" src="/images/pexels-steve-johnson-1266808.png" alt="abstract art" aria-hidden="true">
+                        <img class="about__exhibition-latest__images__image" src="/images/pexels-steve-johnson-1789968.png" alt="abstract art" aria-hidden="true">
                     </div>
-                    <p>
+                    <p class="about__exhibition-latest__description">
                         Our latest exhibition features some of the brightest artistic minds in the space, each bringing their unique perspective and style to the table. From colorful and bold to soft and subtle, these works of art are sure to leave you mesmerized.
                     </p>
                 </div>
 
-                <div>
-                    <p>
+                <div class="about__exhibition-purpose">
+                    <p class="about__exhibition-purpose__description">
                         The goal of this exhibition is to allow you to discover and get lost in the universe of abstract art. It's an opportunity to experience art in a way that's completely open to interpretation and personal expression. With every brush stroke, each artist invites you to explore the beauty of their vision and be inspired by their passion.
                     </p>
-                    <img src="/images/pexels-fiona-art-3631430.png" alt="abstract art" aria-hidden="true">
+                    <img class="about__exhibition-purpose__image" src="/images/pexels-fiona-art-3631430.png" alt="abstract art" aria-hidden="true">
                 </div>
 
-                <div>
-                    <p>
+                <div class="about__exhibition-invitation">
+                    <p class="about__exhibition-invitation__description">
                         Whether you're an art enthusiast or a curious newcomer, this exhibition is a must-see. So come, immerse yourself in the world of abstract art, and discover the magic that lies within. We can't wait to share this experience with you.
-                        <button>Get tickets</button>
+                        <button class="btn btn--yellow btn-lg">Get tickets</button>
                     </p>
 
-                    <img src="/images/pexels-steve-johnson-1070534.png" alt="abstract art" aria-hidden="true">
+                    <img class="about__exhibition-invitation__image" src="/images/pexels-steve-johnson-1070534.png" alt="abstract art" aria-hidden="true">
                 </div>
             </section>
         </main>
-        <footer>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Contact</a></li>
+        <footer class="main-footer">
+            <ul class="main-footer__menu">
+                <li class="main-footer__menu__item"><a href="#">Home</a></li>
+                <li class="main-footer__menu__item"><a href="#">About</a></li>
+                <li class="main-footer__menu__item"><a href="#">Gallery</a></li>
+                <li class="main-footer__menu__item"><a href="#">Contact</a></li>
             </ul>
         </footer>
     </body>
